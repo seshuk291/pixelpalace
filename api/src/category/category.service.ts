5e3a4baf58af63bd2142c09c058a254068e6b1ba
kolli seshukumar <seshuk291@gmail.com>
2023-08-27 11:55:14 +0530

    added category api

--- api/src/category/category.service.ts
+++ api/src/category/category.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) { }
+  
+  create(CreateCategoryDto: CreateCategoryDto) {
+    return this.categoryRepository.save(CreateCategoryDto);
   }
 
   findAll() {
-    return `This action returns all category`;
+    return this.categoryRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.categoryRepository.findOneBy({ id });
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return this.categoryRepository.update(id, updateCategoryDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    try {
+      let category = await this.findOne(id);
+      if (category) {
+        return this.categoryRepository.remove(category);
+      } else {
+        throw new Error("Category not found");
+      }
+    } catch (error) {
+      return error;
+    }
   }
 }
--- api/src/category/entities/category.entity.ts
+++ api/src/category/entities/category.entity.ts
@@ -1,1 +1,16 @@
-export class Category {}
+import { Product } from "src/product/entities/product.entity";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+@Entity("category")
+export class Category {
+    @PrimaryGeneratedColumn()
+    id:number;
+    
+    @Column()
+    name:string;
+    
+    @Column()
+    description:string;
+
+    @ManyToMany(() => Product, product => product.categories)
+    products: Product[]
+}
