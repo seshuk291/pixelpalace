3b67d6375aeac10fdf51af7de9e3322b5a2bc0b1
kolli seshukumar <seshuk291@gmail.com>
2023-08-27 11:55:43 +0530

    updated address entity

--- api/src/address/address.module.ts
+++ api/src/address/address.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { Address } from './entities/address.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Address])],
   controllers: [AddressController],
   providers: [AddressService],
 })
-export class AddressModule {}
+export class AddressModule {
+
+}
--- api/src/address/entities/address.entity.ts
+++ api/src/address/entities/address.entity.ts
@@ -1,1 +1,24 @@
-export class Address {}
+import { Users } from "src/user/entities/user.entity";
+
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("address")
+export class Address {
+    @PrimaryGeneratedColumn()
+    id:number;
+    
+    @Column()
+    fk_user_id:number;
+
+    @Column()
+    street_address:string;
+
+    @Column()
+    city:string;
+
+    @Column()
+    state:string;
+    
+    @Column()
+    zip:string;
+}
