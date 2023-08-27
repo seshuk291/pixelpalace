d34ee6e9ea28f0614bf1683a928e80e51dc38622
kolli seshukumar <seshuk291@gmail.com>
2023-08-27 11:57:15 +0530

    removed product category resources (joining table) as it handled by typeorm

--- api/src/product_category/dto/update-product_category.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductCategoryDto } from './create-product_category.dto';
-
-export class UpdateProductCategoryDto extends PartialType(CreateProductCategoryDto) {}
